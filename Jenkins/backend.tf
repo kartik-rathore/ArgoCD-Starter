36680d67ed3f68ad0dd59ca3b74eb0980cab48af
Kartik Singh Rathore <166915596+kartik-rathore@users.noreply.github.com>
2024-07-02 15:55:26 +0530

    Update backend.tf

--- Jenkins/backend.tf
+++ Jenkins/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-eks-jenkins"
+    bucket = "terraform-eks-jenkins-kartik"
     key    = "jenkins/terraform.tfstate"
     region = "us-east-1"
   }
